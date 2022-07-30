37c3c735f9aff8ee77b6b46122ba1e73db2ce14d
sulakshya <65058644+sulakshya1@users.noreply.github.com>
2022-07-30 16:07:05 +0545

    Add files via upload

--- 2019_spring3b.cpp
+++ 2019_spring3b.cpp
@@ -3,23 +3,26 @@ using namespace std;
 class Derived;
 class Base{
     public:
-    int x,y;
+    int x,y,m,n;
     void get_data(){
         cout<<"enter two vector co-ordinates\n";
         cin>>x>>y;
+        cout<<"enter two vector co-ordinates\n";
+        cin>>m>>n;
     }
     friend void display(Base,Derived);
 };
 class Derived:public Base{
     public:
-    int sum;
+    int sum1,sum2;
     void add_vector(){
-        sum=x+y;
+        sum1=x+m;
+        sum2=y+n;
     }
     friend void display(Base,Derived);
 };
 void display(Base b,Derived d){
-    cout<<"sum:"<<d.sum;
+    cout<<"sum:"<<d.sum1<<"i+"<<d.sum2<<"j"<<endl;
 }
 int main(){
     Base b;
