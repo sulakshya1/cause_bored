b6d7948f35481793bc430c6dc076b3ddd5f5a4b8
sulakshya <65058644+sulakshya1@users.noreply.github.com>
2022-08-02 17:05:07 +0545

    Add files via upload

--- 2017_spring3b.cpp
+++ 2017_spring3b.cpp
@@ -9,11 +9,14 @@ class time{
         min=b;
         sec=c;
     }
-    time operator+(time t2){
+    void display(){
+        cout<<"time:"<<hour<<":"<<min<<":"<<sec<<endl;
+    }
+    void add(time t1,time t2){
         time t3;
-        t3.hour=hour+t2.hour;
-        t3.min=min+t2.min;
-        t3.sec=sec+t2.sec;
+        t3.hour=t1.hour+t2.hour;
+        t3.min=t1.min+t2.min;
+        t3.sec=t1.sec+t2.sec;
         if(t3.sec>60){
             while (t3.sec>60)
             {
@@ -33,16 +36,12 @@ class time{
             t3.hour=t3.hour-24;
             }
         }
-        return t3;
-    }
-    void display(){
-        cout<<"time:"<<hour<<":"<<min<<":"<<sec<<endl;
+        t3.display();
     }
 };
 int main(){
     time t1(2,55,55),t2(5,10,100);
     time t3;
-    t3=t1+t2;
-    t3.display();
+    t3.add(t1,t2);
     return 0;
 }
